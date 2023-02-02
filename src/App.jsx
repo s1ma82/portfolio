577a3350701a7ddb880076de4acb20f28f2c8a59
Daniil Simanovich <s1mahochukvas@vk.com>
2023-02-02 13:11:15 +0300

    works

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import dedent from 'dedent'
 import Home from "./pages/Home/Home"
 import Header from "./layout/Header/Header"
-import Works from "./pages/Works/Works"
+// import Works from "./pages/Works/Works"
 import { ContextMenu } from "./components"
 
 export default () => {
