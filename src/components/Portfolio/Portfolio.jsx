4b1e1d18cc33565b22d9b94ae2f586e98ec87a9c
s1ma82 <s1mahochukvas@vk.com>
2023-04-26 21:49:53 +0300

    portfolio works update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,20 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+import useOutside from '@/hooks/useOutside'
 import { works } from '../../assets/works'
 import {Title, Text, Context} from '../../ui'
 import styles from './Portfolio.module.scss'
 import { Link } from 'react-router-dom';
+
 export default () => {
+
+
+	const Work = (data) => {
+
+		const [state, setState] = useOutside([`[data-focus="${data.name}"]`])
+
+
+		return ( 
+			<li
+				
+				data-focus={data.name}
+				className={`${styles.works__item} ${state ? styles.works__item_active : ''}`}
+				onClick={() => setState(!state)}
+			>
+				<div className={styles.content}>
+					<Title className={styles.title}>{data.name}</Title>
+					<img src={data.screens[0]} alt="image" />
+				</div>
+				<div  className={`${styles.descr} ${state ? styles.descr_active : ''}`}>
+					<Text>{data.descr}</Text>
+				</div>
+			</li>
+		)
+
+
+	}
 	return (
 		<div className={styles.portfolio}>
 			<ul className={styles.works}>
 				{works?.map((data, n) => (
-					<Context key={`works__item:${n}`}  tag={`portfolio-work #${n}`}>
-						<li className={styles.works__item}>
-							<Title className={styles.title}>{data.name}</Title>
-							<Link className={styles.link} to={`/works/${data.name}`}>
-								<img src={data.screens[0]} alt="" />
-							</Link>
-							<Text className={styles.descr}>{data.descr}</Text>
-						</li>
+					<Context key={`works__item:${n}`} tag={`portfolio-work #${n}`}>
+						<Work {...data}/>
 					</Context>
 				))}
 			</ul>
